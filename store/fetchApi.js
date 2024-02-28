66423834b9578b3261cfc3002a543ab8a266ff71
HoutSovannarith <hout.sovannarith2000@gmail.com>
2024-02-28 15:51:06 +0700

    First Commit Project

---
+++ store/fetchApi.js
@@ -0,0 +1,16 @@
+const BASE_URL = "http://localhost:63342/data/";
+
+export async function getData(endPoint){
+    try{
+        const res = await fetch(BASE_URL + endPoint, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        const data = await res.json();
+        return data;
+    }catch (error){
+        console.log(error);
+    }
+}
